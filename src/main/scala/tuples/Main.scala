5d57e60fd10b5047c96dc89f93a6fd88b8c4ec1f
Matheus <matheus.ggoes@outlook.com>
2021-04-02 14:19:55 -0300

    Add tuple examples

---
+++ src/main/scala/tuples/Main.scala
@@ -0,0 +1,39 @@
+package tuples
+
+object Main extends App {
+
+  /**
+   * Tuple - Is a value that contains a fixed number of elements
+   */
+  val ingredient = ("Sugar", 25)
+  println(ingredient)
+
+  val person = ("Matheus", 28, 1.90)
+  println(person._1)
+  println(person._2)
+  println(person._3)
+
+  /**
+   * Pattern matching on tuples
+   */
+  val (name, quantity) = ingredient // Decomposing a tuple
+  println(name)
+  println(quantity)
+
+  val planets = List(
+    ("Mercury", 57.9),
+    ("Venus", 108.2),
+    ("Earth", 149.6),
+    ("Mars", 227.9),
+    ("Jupter", 778.3),
+  )
+  planets.foreach {
+    case ("Earth", distance) => println(s"Earth: $distance")
+    case _ =>
+  }
+
+  val pairs = List((1, 2), (3, 4), (5, 6), (7, 8), (9, 0))
+  for ((a, b) <- pairs) {
+    println(s"$a, $b")
+  }
+}
